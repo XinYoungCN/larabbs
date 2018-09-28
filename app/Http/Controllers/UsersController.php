41dfc24a7dd39d7d3c502a2f1cbd63d7123f5d59
elliot <colinmcrae186@hotmail.com>
2018-09-28 10:58:51 +0000

    Finish Authorized

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,16 +11,33 @@ use App\Handlers\ImageUploadHandler;
 class UsersController extends Controller
 {
     //
+
+    public function __construct()
+    {
+        $this -> middleware('auth',['except' =>['show']]);
+        /*__construct 是 PHP 的构造器方法，当一个类对象被创建之前该方法将会被调用。
+        我们在 __construct 方法中调用了 middleware 方法，
+        该方法接收两个参数，第一个为中间件的名称，第二个为要进行过滤的动作。
+        我们通过 except 方法来设定 指定动作 不使用 Auth 中间件进行过滤，
+        意为 —— 除了此处指定的动作以外，所有其他动作都必须登录用户才能访问，
+        类似于黑名单的过滤机制。相反的还有 only 白名单方法，将只过滤指定动作。
+        我们提倡在控制器 Auth 中间件使用中，首选 except 方法，这样的话
+        ，当你新增一个控制器方法时，默认是安全的，此为最佳实践。
+        */
+    }
+
     public function show(User $user){
         return view('users.show',compact('user'));
     }
 
     public function edit(User $user){
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user){
 
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
+        \App\Models\User::class  => \App\Policies\UserPolicy::class, //对用户模型User进行授权策略注册
     ];
 
     /**
