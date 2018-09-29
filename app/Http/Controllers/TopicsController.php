0dc66e4fa94bb1a053500ae4c912fa372c3d4cc6
elliot <colinmcrae186@hotmail.com>
2018-09-29 03:58:27 +0000

    Topic order

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -14,12 +14,11 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index()
-	{
-        $topics = Topic::with('user', 'category')->paginate(30);  //方法 with() 提前加载了我们后面需要用到的关联属性 user 和 category，并做了缓存。后面即使是在遍历数据时使用到这两个关联属性，数据已经被预加载并缓存，因此不会再产生多余的 SQL 查询：
+    public function index(Request $request, Topic $topic)
+    {
+        $topics = $topic->withOrder($request->order)->paginate(20);
         return view('topics.index', compact('topics'));
-	}
-
+    }
     public function show(Topic $topic)
     {
         return view('topics.show', compact('topic'));
