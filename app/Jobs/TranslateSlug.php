d36aa1f1be8db89bf6ba08e620d9e5cd9c93fab3
elliot <colinmcrae186@hotmail.com>
2018-09-30 09:47:11 +0000

    Add queue for translate slug

--- app/Jobs/TranslateSlug.php
+++ app/Jobs/TranslateSlug.php
@@ -2,16 +2,17 @@
 
 namespace App\Jobs;
 
+use App\Handlers\SlugTranslateHandler;
 use App\Models\Topic;
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-
 class TranslateSlug implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    protected   $topic;
 
     /**
      * Create a new job instance.
